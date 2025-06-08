d650195736a337867036d3597eca4a036b8c5e72
Serjo Bykov <serjobikov@gmail.com>
2025-06-08 20:11:34 +0300

    uodateApp(IosPathwayTests)

--- IOS Pathway/IOS PathwayTests/IOS_PathwayTests.swift
+++ IOS Pathway/IOS PathwayTests/IOS_PathwayTests.swift
@@ -11,7 +11,7 @@ import Testing
 struct IOS_PathwayTests {
 
     @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        //Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
 
 }
